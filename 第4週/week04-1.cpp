50c305683b6116d38ed3510293166f6c03ffbe9b
0706vera <vera0706123456@gmail.com>
2025-03-14 12:06:13 +0800

    Add files via upload

---
+++ 第4週/week04-1.cpp
@@ -0,0 +1,14 @@
+//week04-1.cpp   C++ セ 璶糶︹柑祘Α
+//LeetCode 2529. Maximum Count of Positive Integer and Nagative Integer
+class Solution {
+public:
+    int maximumCount(vector<int>& nums) {
+        int pos = 0,neg = 0;
+        for(int i=0; i< nums.size(); i++) {
+            if(nums [i] > 0) pos++; //タ计++
+            if(nums [i] < 0) neg++; //璽计++
+        }
+        if( pos > neg ) return pos; //タ计ゑ耕 癳
+        else return neg; //ぃ礛碞璽计ゑ耕 癳
+    }
+};
