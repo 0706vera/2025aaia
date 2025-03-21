1706147e9c276834d4fdecd89d3f950d71f91860
0706vera <vera0706123456@gmail.com>
2025-03-21 10:13:28 +0800

    Delete week03 directory

--- week03/week03-1.cpp
+++
@@ -1,7 +0,0 @@
-class Solution {
-public:
-    long long coloredCells(int n) {
-        long long ans = n;//要多寫這行，把數字升級，變64位元「很長很長的整數」
-        return ans*ans + (ans-1)*(ans-1);//C/C++/Java/C#都要加分號        
-    }
-}
